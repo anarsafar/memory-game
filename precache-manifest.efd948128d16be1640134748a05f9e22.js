b21053866534e16c67d873dd082ad2a5ac2e8bc8
Anar Seferov <1anarseferov@gmail.com>
2020-12-04 19:24:07 +0400

    Updates

---
+++ precache-manifest.efd948128d16be1640134748a05f9e22.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "916f5cdc154fcf345b2531649b38794b",
+    "url": "/memory-game/index.html"
+  },
+  {
+    "revision": "3892484a65233705d8d2",
+    "url": "/memory-game/static/css/main.644c220d.chunk.css"
+  },
+  {
+    "revision": "60bdcbf73c2f8b04e8cf",
+    "url": "/memory-game/static/js/2.6ef03000.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/memory-game/static/js/2.6ef03000.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3892484a65233705d8d2",
+    "url": "/memory-game/static/js/main.8ccd1ad7.chunk.js"
+  },
+  {
+    "revision": "edbde91f6abb6e69834c",
+    "url": "/memory-game/static/js/runtime-main.a1f811c6.js"
+  },
+  {
+    "revision": "4c243461b7a019ecbfde66bdb3ac3722",
+    "url": "/memory-game/static/media/Luffy.4c243461.png"
+  },
+  {
+    "revision": "56e3e48d4357ce56280632dd2a1672c2",
+    "url": "/memory-game/static/media/Nami.56e3e48d.png"
+  },
+  {
+    "revision": "dd555cc40db208da9c59c493c7355885",
+    "url": "/memory-game/static/media/Sanji.dd555cc4.png"
+  },
+  {
+    "revision": "6a39bfa7f00a790623c20e6b98c94f88",
+    "url": "/memory-game/static/media/Usopp.6a39bfa7.png"
+  },
+  {
+    "revision": "67381f9a976286558762024a19112334",
+    "url": "/memory-game/static/media/brooks.67381f9a.png"
+  },
+  {
+    "revision": "4f52850f675c7ec24cd7996c1136323b",
+    "url": "/memory-game/static/media/onepiece.4f52850f.png"
+  },
+  {
+    "revision": "a90fd395be33d4c5304d4ef01cd4bc0f",
+    "url": "/memory-game/static/media/zorro.a90fd395.png"
+  }
+]);
